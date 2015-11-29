d031f660bc233a3e228602fc429ab21332644606
JordanCollier <jordanicollier@gmail.com>
2015-11-29 13:42:31 -0700

    add solutions and notes for chapter 2

---
+++ Chapter-2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// this is a comment
+
+func main() {
+	fmt.Println("Hello, my name is Jordan")
+}
+
+/*
+Great point to read out loud:
+
+Create a new executable program, which references the fmt library and contains
+one function called main. That function takes no arguments, doesn't return
+anything and does the following: Access the Println function contained inside of
+ the fmt package and invoke it using one argument – the string Hello World.
+*/
